1b4e0a9082bb5b2b2bd5495e77de4221db1df5c8
Artsiom Kukharev EPAM <artsiom_kukharev@epam.com>
2017-12-01 19:00:43 +0300

    functions/feature: init

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,13 @@
 const functions = require('firebase-functions');
 
+exports.detectMeaging = functions.firestore
+  .document('FCMMessaging/{user.uid}').onUpdate(event => {
+    
+  });
+
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
-//
+//∫unctionaλ
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
