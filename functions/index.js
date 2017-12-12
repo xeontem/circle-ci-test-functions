79de935c0ccc9bb2603de17040f3f6822dd6e221
xeontem <xeontem@gmail.com>
2017-12-13 00:05:01 +0300

    func/fix: fcm is working!

--- functions/index.js
+++ functions/index.js
@@ -1,71 +1,58 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 
-// const currentId
-// console.log(functions.config());
 admin.initializeApp(functions.config().firebase);
-
-// const currentUser = {};
-
-// exports storeCurrentUserID = functions.firestore.document('FCMMesaging/{newId}')
-//   .onCreate(user => {
-//     console.log(user);
-//     currentUser = user;
-//   });
-
-exports.fcmSendcreate = functions.database.ref('cources/{generatedId}')
+// const bucket = admin.storage().bucket();
+// for(let key in bucket) {
+//           console.log('key: ', key);
+//           console.log('value: ', bucket[key]);
+//         }
+//-------------------------- firestore -----------------------------------
+exports.fsSendcreate = functions.firestore.document('/cources/{generatedId}')
   .onCreate(cource => {
-    console.log(JSON.stringify(cource));
-    // const message = event.data.val();
-    // const userId = event.params.userId;
+    // console.log(JSON.stringify(cource));
+
+    // var newValue = cource.data.data();
+    // console.log(JSON.stringify(newValue));
+
+    // var previousValue = cource.data.previous ? cource.data.previous.data() : cource.data.previous;
+    // console.log(JSON.stringify(previousValue));
+    // console.log(cource.params);// what in curly brackets in .document method arguments
     const payload = {
       notification: {
         title: 'adding new cource.',
-        body: 'new cource successfully added!'
+        body: `cource ${cource.title} successfully added!`
         // icon:
       }
     };
-
-    // admin.messaging().sendToDevice(currentUser., payload);
-    //   .then(res => console.log('sent successfully', res))
-    //   .catch(err => console.log(err));
-    // admin.database().ref('FCMMesaging/').once('value')
-    //   .then(token => console.log(token))
-    //   .then(userFcmToken => {
-    //     console.log(userFcmToken);
-    //     admin.messaging().sendToDevice(userFcmToken, payload);
-    //   })
+    // functions.firestore.document('/FCMMessaging/{uid}')
+    // admin.database().ref('/users').once('value').then(obj => {
+      admin.firestore().collection('users').get()
+        .then(obj => obj.docs)
+        .then(docs => docs.map(ref => {
+          if(ref._fieldsProto.token) {
+            const token = ref._fieldsProto.token.stringValue
+            admin.messaging().sendToDevice(token, payload)
+          }
+        }))
+        // console.log('-----------------------------------------');
+        // docs.map(ref => {
+        //   console.log(ref._fieldsProto.token && ref._fieldsProto.token.stringValue);
+        // })
+        // for(let key in obj.docs) {
+        //   console.log('key: ', key);
+        //   console.log('value: ', obj.docs[key]);
+        // }
+        // console.log('-----------------------------------------');
+        
+      // })
+    // });
+    // admin.database().ref('/FCMMessaging/').once('value')
+      // .then(obj => {
+      //   console.log(obj.val());
+      //   token.val()
+      // })
+      // .then(userFcmToken => {
+      // });
+      return false;
   });
-
-  exports.fcmSendupdate = functions.database.ref('cources')
-  .onUpdate(cource => {
-    console.log(JSON.stringify(cource));
-    // const message = event.data.val();
-    // const userId = event.params.userId;
-    const payload = {
-      notification: {
-        title: 'adding new cource.',
-        body: 'new cource successfully added!'
-        // icon:
-      }
-    };
-
-    // admin.messaging().sendToDevice(currentUser., payload);
-    //   .then(res => console.log('sent successfully', res))
-    //   .catch(err => console.log(err));
-    // admin.database().ref('FCMMesaging/').once('value')
-    //   .then(token => console.log(token))
-    //   .then(userFcmToken => {
-    //     console.log(userFcmToken);
-    //     admin.messaging().sendToDevice(userFcmToken, payload);
-    //   })
-  });
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//∫unctionaλ
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
-
-// firebase.apiKey="AIzaSyBZKj2TXF1S12liuUSV7uILNqqzq1jnQlc" firebase.authDomain="circle-ci-test-31dfc.firebaseapp.com" firebase.databaseURL="https://circle-ci-test-31dfc.firebaseio.com" firebase.projectId="circle-ci-test-31dfc" firebase.storageBucket="circle-ci-test-31dfc.appspot.com" firebase.messagingSenderId="350049236638"
