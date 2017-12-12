cd04cffe1c3dd47923829db0ea735525e7d07c32
Artsiom Kukharev EPAM <artsiom_kukharev@epam.com>
2017-12-12 18:06:50 +0300

    func/feature: watchers are not working

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,64 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-exports.detectMeaging = functions.firestore
-  .document('FCMMessaging/{user.uid}').onUpdate(event => {
-    
+// const currentId
+// console.log(functions.config());
+admin.initializeApp(functions.config().firebase);
+
+// const currentUser = {};
+
+// exports storeCurrentUserID = functions.firestore.document('FCMMesaging/{newId}')
+//   .onCreate(user => {
+//     console.log(user);
+//     currentUser = user;
+//   });
+
+exports.fcmSendcreate = functions.database.ref('cources/{generatedId}')
+  .onCreate(cource => {
+    console.log(JSON.stringify(cource));
+    // const message = event.data.val();
+    // const userId = event.params.userId;
+    const payload = {
+      notification: {
+        title: 'adding new cource.',
+        body: 'new cource successfully added!'
+        // icon:
+      }
+    };
+
+    // admin.messaging().sendToDevice(currentUser., payload);
+    //   .then(res => console.log('sent successfully', res))
+    //   .catch(err => console.log(err));
+    // admin.database().ref('FCMMesaging/').once('value')
+    //   .then(token => console.log(token))
+    //   .then(userFcmToken => {
+    //     console.log(userFcmToken);
+    //     admin.messaging().sendToDevice(userFcmToken, payload);
+    //   })
+  });
+
+  exports.fcmSendupdate = functions.database.ref('cources')
+  .onUpdate(cource => {
+    console.log(JSON.stringify(cource));
+    // const message = event.data.val();
+    // const userId = event.params.userId;
+    const payload = {
+      notification: {
+        title: 'adding new cource.',
+        body: 'new cource successfully added!'
+        // icon:
+      }
+    };
+
+    // admin.messaging().sendToDevice(currentUser., payload);
+    //   .then(res => console.log('sent successfully', res))
+    //   .catch(err => console.log(err));
+    // admin.database().ref('FCMMesaging/').once('value')
+    //   .then(token => console.log(token))
+    //   .then(userFcmToken => {
+    //     console.log(userFcmToken);
+    //     admin.messaging().sendToDevice(userFcmToken, payload);
+    //   })
   });
 
 // // Create and Deploy Your First Cloud Functions
@@ -11,3 +67,5 @@ exports.detectMeaging = functions.firestore
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+// firebase.apiKey="AIzaSyBZKj2TXF1S12liuUSV7uILNqqzq1jnQlc" firebase.authDomain="circle-ci-test-31dfc.firebaseapp.com" firebase.databaseURL="https://circle-ci-test-31dfc.firebaseio.com" firebase.projectId="circle-ci-test-31dfc" firebase.storageBucket="circle-ci-test-31dfc.appspot.com" firebase.messagingSenderId="350049236638"
