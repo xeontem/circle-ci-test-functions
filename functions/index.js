0df255c23750a6bb2d32773bcd1de34b69560724
Artsiom Kukharev EPAM <artsiom_kukharev@epam.com>
2017-12-13 11:45:16 +0300

    func/fix: refactoring listeners

--- functions/index.js
+++ functions/index.js
@@ -10,104 +10,50 @@ admin.initializeApp(functions.config().firebase);
 //-------------------------- firestore -----------------------------------
 exports.fsSendcreate = functions.firestore.document('/cources/{courceID}')
   .onCreate(cource => {
-    // console.log(JSON.stringify(cource));
-
-    var newValue = cource.data.data();
-    // console.log(JSON.stringify(newValue));
-
-    // var previousValue = cource.data.previous ? cource.data.previous.data() : cource.data.previous;
-    // console.log(JSON.stringify(previousValue));
-    // console.log(cource.params);// what in curly brackets in .document method arguments
+    const newValue = cource.data.data();
     const payload = {
       notification: {
         title: 'Circle CI test',
-        body: `cource ${newValue.title} successfully added!`
-        // icon:
+        body: `cource ${newValue.title} successfully added!`,
+        icon: 'https://firebasestorage.googleapis.com/v0/b/circle-ci-test-31dfc.appspot.com/o/firestore.png?alt=media&token=a1227c5e-6cad-4dfb-81d6-ae07d7dbac1c'
       }
     };
-        console.log('-----------------------------------------');
+    console.log('-----------------------------------------');
     console.log(`cource ${newValue.title} successfully added!`);
-    // functions.firestore.document('/FCMMessaging/{uid}')
-    // admin.database().ref('/users').once('value').then(obj => {
-      admin.firestore().collection('users').get()
-        .then(obj => obj.docs)
-        .then(docs => docs.map(ref => {
-          if(ref._fieldsProto.token) {
-            const token = ref._fieldsProto.token.stringValue;
-            console.log(token);
-            admin.messaging().sendToDevice(token, payload)
-          }
-            console.log('-----------------------------------------');
-        }))
-        // docs.map(ref => {
-        //   console.log(ref._fieldsProto.token && ref._fieldsProto.token.stringValue);
-        // })
-        // for(let key in obj.docs) {
-        //   console.log('key: ', key);
-        //   console.log('value: ', obj.docs[key]);
-        // }
-        // console.log('-----------------------------------------');
-        
-      // })
-    // });
-    // admin.database().ref('/FCMMessaging/').once('value')
-      // .then(obj => {
-      //   console.log(obj.val());
-      //   token.val()
-      // })
-      // .then(userFcmToken => {
-      // });
-      return false;
+    
+    admin.firestore().collection('users').get()
+      .then(snap => snap.docs)
+      .then(docs => docs.map(doc => doc.data()))
+      .then(users => users.filter(user => !!user.token))
+      .then(users => users.map(user => {
+          console.log(`sending message to user: ${user.uid}`);
+          admin.messaging().sendToDevice(user.token, payload);
+      }))
+      .then(x => console.log('-----------------------------------------'));
+    return false;
   });
 
-  exports.fsSenddelete = functions.firestore.document('/cources/{courceID}')
-  .onDelete(cource => {
-    // console.log(JSON.stringify(cource));
-
-    // var newValue = cource.data.data();
-    // console.log(JSON.stringify(newValue));
-
-    var previousValue = cource.data.previous ? cource.data.previous.data() : cource.data.previous;
-    // console.log(JSON.stringify(previousValue));
-    // console.log(cource.params);// what in curly brackets in .document method arguments
-    const payload = {
-      notification: {
-        title: 'Circle CI test',
-        body: `cource ${previousValue.title} successfully deleted!`
-        // icon:
-      }
-    };
-        console.log('-----------------------------------------');
-    console.log(`cource ${previousValue.title} successfully deleted!`);
-    // functions.firestore.document('/FCMMessaging/{uid}')
-    // admin.database().ref('/users').once('value').then(obj => {
-      admin.firestore().collection('users').get()
-        .then(obj => obj.docs)
-        .then(docs => docs.map(ref => {
-          if(ref._fieldsProto.token) {
-            const token = ref._fieldsProto.token.stringValue;
-            console.log(token);
-            admin.messaging().sendToDevice(token, payload)
-          }
-            console.log('-----------------------------------------');
-        }))
-        // docs.map(ref => {
-        //   console.log(ref._fieldsProto.token && ref._fieldsProto.token.stringValue);
-        // })
-        // for(let key in obj.docs) {
-        //   console.log('key: ', key);
-        //   console.log('value: ', obj.docs[key]);
-        // }
-        // console.log('-----------------------------------------');
-        
-      // })
-    // });
-    // admin.database().ref('/FCMMessaging/').once('value')
-      // .then(obj => {
-      //   console.log(obj.val());
-      //   token.val()
-      // })
-      // .then(userFcmToken => {
-      // });
-      return false;
-  });
+exports.fsSenddelete = functions.firestore.document('/cources/{courceID}')
+.onDelete(cource => {
+  const previousValue = cource.data.previous.data();
+  const payload = {
+    notification: {
+      title: 'Circle CI test',
+      body: `cource ${previousValue.title} successfully deleted!`,
+      icon: 'https://firebasestorage.googleapis.com/v0/b/circle-ci-test-31dfc.appspot.com/o/firestore.png?alt=media&token=a1227c5e-6cad-4dfb-81d6-ae07d7dbac1c'
+    }
+  };
+  console.log('-----------------------------------------');
+  console.log(`cource ${previousValue.title} successfully deleted!`);
+  
+  admin.firestore().collection('users').get()
+    .then(snap => snap.docs)
+    .then(docs => docs.map(doc => doc.data()))
+    .then(users => users.filter(user => !!user.token))
+    .then(users => users.map(user => {
+        console.log(`sending message to user: ${user.uid}`);
+        admin.messaging().sendToDevice(user.token, payload);
+    }))
+    .then(x => console.log('-----------------------------------------'));
+  return false;
+});
